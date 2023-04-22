101d8f9a7989253e8004764a5fa6de39032ec887
DAP <disap332@gmail.com>
2023-04-21 21:09:12 -0400

    added bootstrap and react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import firebase from 'firebase/compat/app'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBy3Vp3L1l_HtsztZMdLXBuVlL99yXvA4M",
+  authDomain: "draken-jewelry.firebaseapp.com",
+  projectId: "draken-jewelry",
+  storageBucket: "draken-jewelry.appspot.com",
+  messagingSenderId: "969793291765",
+  appId: "1:969793291765:web:adcb01185be7c34a4e8cbc",
+  measurementId: "G-9RYVGZBP6J"
+})
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
